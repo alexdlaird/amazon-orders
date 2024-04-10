80badcfbee2b753ca5f1039ca568be181b9b6ee8
Alex Laird <alaird@twilio.com>
2024-04-09 21:53:08 -0500

    Finalize minor updates for 1.1.0 release.

--- amazonorders/cli.py
+++ amazonorders/cli.py
@@ -60,7 +60,7 @@ def amazon_orders_cli(ctx: Context,
     amazon-orders is an unofficial library that provides a command line interface alongside a programmatic API that
     can be used to interact with Amazon.com's consumer-facing website.
 
-    This works by parsing website data from Amazon.com. A nightly build validates functionality to ensure its
+    This works by parsing website data from Amazon.com. A periodic build validates functionality to ensure its
     stability, but as Amazon provides no official API to use, this package may break at any time. This
     package only supports the English version of the website.
 
