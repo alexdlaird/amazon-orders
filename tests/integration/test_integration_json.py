54388c5d94a581d5ce21c2d736d299cd4e3ad7dc
Alex Laird <alaird@twilio.com>
2024-05-18 18:18:07 -0700

    Fix paramterized tests with pytest.

--- tests/integration/test_integration_json.py
+++ tests/integration/test_integration_json.py
@@ -3,14 +3,25 @@ __license__ = "MIT"
 
 import json
 import os
-import unittest
 from datetime import datetime
+
+from parameterized import parameterized
 
 from tests.integrationtestcase import IntegrationTestCase
 
 PRIVATE_RESOURCES_DIR = os.path.normpath(
     os.path.join(os.path.abspath(os.path.dirname(__file__)),
                  "private-resources"))
+
+private_json_file_data = []
+if os.path.exists(PRIVATE_RESOURCES_DIR):
+    for filename in os.listdir(PRIVATE_RESOURCES_DIR):
+        if filename == ".gitignore":
+            continue
+
+        with open(os.path.join(PRIVATE_RESOURCES_DIR, filename), "r", encoding="utf-8") as f:
+            data = json.loads(f.read())
+            private_json_file_data.append((filename, data))
 
 
 class TestIntegrationJSON(IntegrationTestCase):
@@ -76,25 +87,20 @@ class TestIntegrationJSON(IntegrationTestCase):
     define here the fields and values under the ``Order`` that you want to assert on.
     """
 
-    def __init__(self, method_name, filename=None, data=None):
-        super(TestIntegrationJSON, self).__init__(method_name)
-
-        self.filename = filename
-        self.data = data
-
-    def run_json_test(self):
-        print(f"Info: Dynamic test is running from JSON file {self.filename}")
+    @parameterized.expand(private_json_file_data)
+    def test_json(self, filename, data):
+        print(f"Info: Dynamic test is running from JSON file {filename}")
 
         # GIVEN
-        func = self.data.pop("func")
+        func = data.pop("func")
 
         if func == "get_order_history":
-            order_len = self.data.pop("orders_len")
-            orders_json = self.data.pop("orders")
-            full_details = self.data.get("full_details")
+            order_len = data.pop("orders_len")
+            orders_json = data.pop("orders")
+            full_details = data.get("full_details")
 
             # WHEN
-            orders = self.amazon_orders.get_order_history(**self.data)
+            orders = self.amazon_orders.get_order_history(**data)
 
             # THEN
             self.assertEqual(order_len, len(orders))
@@ -103,7 +109,7 @@ class TestIntegrationJSON(IntegrationTestCase):
                 self.assertEqual(order.full_details, full_details)
                 self.assert_json_items(order, order_json)
         elif func == "get_order":
-            order_json = self.data
+            order_json = data
             order_id = order_json["order_number"]
 
             # WHEN
@@ -114,7 +120,7 @@ class TestIntegrationJSON(IntegrationTestCase):
             self.assert_json_items(order, order_json)
         else:
             self.fail(
-                f"Unknown function AmazonOrders. {func}, check JSON in test file {self.filename}")
+                f"Unknown function AmazonOrders. {func}, check JSON in test file {filename}")
 
     def assert_json_items(self, entity, json_dict):
         for json_key, json_value in json_dict.items():
@@ -136,18 +142,3 @@ class TestIntegrationJSON(IntegrationTestCase):
                         datetime.strptime(json_value, "%Y-%m-%d").date(), entity_attr)
                 except (TypeError, ValueError):
                     self.assertEqual(json_value, entity_attr)
-
-
-def load_tests(loader, tests, pattern):
-    test_cases = unittest.TestSuite()
-    if os.path.exists(PRIVATE_RESOURCES_DIR):
-        for filename in os.listdir(PRIVATE_RESOURCES_DIR):
-            if filename == ".gitignore":
-                continue
-
-            with open(os.path.join(PRIVATE_RESOURCES_DIR, filename), "r",
-                      encoding="utf-8") as f:
-                data = json.loads(f.read())
-                test_cases.addTest(
-                    TestIntegrationJSON("run_json_test", filename, data))
-    return test_cases
