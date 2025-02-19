d435acefe9587dbea18a2d2ede47d751b1261316
Jeffrey Wu <jeffreywu1996@gmail.com>
2025-02-19 15:30:11 -0800

    fix: Added extra selectors for order num and date

--- amazonorders/selectors.py
+++ amazonorders/selectors.py
@@ -90,6 +90,7 @@ class Selectors:
 
     FIELD_ORDER_DETAILS_LINK_SELECTOR = "a.yohtmlc-order-details-link"
     FIELD_ORDER_NUMBER_SELECTOR = ["[data-component='briefOrderInfo'] div.a-column",
+                                   "[data-component='orderId']",
                                    ".order-date-invoice-item bdi[dir='ltr']",
                                    "bdi[dir='ltr']",
                                    "span[dir='ltr']"]
@@ -97,8 +98,10 @@ class Selectors:
                                         "div.order-header div.a-column.a-span2",
                                         "div.order-header div.a-col-left .a-span9"]
     FIELD_ORDER_PLACED_DATE_SELECTOR = ["[data-component='briefOrderInfo'] div.a-column",
+                                        "[data-component='orderDate']",
                                         "span.order-date-invoice-item",
                                         "div.a-span3"]
+
     FIELD_ORDER_PAYMENT_METHOD_SELECTOR = "img.pmts-payment-credit-card-instrument-logo"
     FIELD_ORDER_PAYMENT_METHOD_LAST_4_SELECTOR = "span:has(img.pmts-payment-credit-card-instrument-logo):last-child"
     FIELD_ORDER_SUBTOTALS_TAG_ITERATOR_SELECTOR = ["[data-component='orderSubtotals'] div.a-row",
