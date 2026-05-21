c2518b8925a04177b658c08d76fdd81679e038c4
Jeffrey Chang <jeffchang192@gmail.com>
2026-05-21 09:08:13 -0400

    Parse item prices from offscreen unit price

--- tests/unit/entity/test_item.py
+++ tests/unit/entity/test_item.py
@@ -40,6 +40,55 @@ class TestItem(UnitTestCase):
         self.assertEqual(item.title, "Item Title")
         self.assertEqual(item.price, 1234.99)
 
+    def test_price_from_order_details_unit_price_offscreen(self):
+        # GIVEN
+        html = """
+<div class="a-fixed-left-grid a-spacing-none">
+    <div data-component="itemTitle">
+        <a href="/dp/B0CFPJYX7P?ref_=ppx_hzod_title_dt_b_fed_asin_title_0_0">
+            All-new Amazon Kindle Paperwhite
+        </a>
+    </div>
+    <div data-component="unitPrice">
+        <span class="a-price a-text-price" data-a-size="s" data-a-color="base">
+            <span class="a-offscreen">$99.99</span>
+            <span aria-hidden="true">$99.99</span>
+        </span>
+    </div>
+</div>
+"""
+        parsed = BeautifulSoup(html, self.test_config.bs4_parser)
+
+        # WHEN
+        item = Item(parsed, self.test_config)
+
+        # THEN
+        self.assertEqual(item.title, "All-new Amazon Kindle Paperwhite")
+        self.assertEqual(item.price, 99.99)
+
+    def test_title_and_link_from_history_image_card(self):
+        # GIVEN
+        html = """
+<div class="a-fixed-right-grid-col item-box" style="float:left;">
+    <div class="product-grid-image">
+        <a class="a-link-normal" href="/dp/B0CFPJYX7P?ref=ppx_yo2ov_dt_b_fed_asin_title" tabindex="-1">
+            <img alt="All-new Amazon Kindle Paperwhite"
+                 src="https://m.media-amazon.com/images/I/example._SS142_.jpg" />
+        </a>
+    </div>
+</div>
+"""
+        parsed = BeautifulSoup(html, self.test_config.bs4_parser)
+
+        # WHEN
+        item = Item(parsed, self.test_config)
+
+        # THEN
+        self.assertEqual(item.title, "All-new Amazon Kindle Paperwhite")
+        self.assertEqual(
+            item.link,
+            "https://www.amazon.com/dp/B0CFPJYX7P?ref=ppx_yo2ov_dt_b_fed_asin_title")
+
     def test_title_starts_with_ampersand_use_lxml(self):
         # GIVEN
         lxml_config = AmazonOrdersConfig(data={
